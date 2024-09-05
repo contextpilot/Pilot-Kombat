e19f748dbb6547dfad069ddfb6e706453fd4bb46
Pilot Context <pilot.context@cryptitalk.com>
2024-09-05 23:42:57 +0000

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 8080,
-    host: '0.0.0.0' // Ensure the server listens on all network interfaces.
-  },
-  preview: {
-    port: 8080,
-    host: '0.0.0.0' // Ensure the preview server does the same.
-  }
-});
+  plugins: [react()],
+})
