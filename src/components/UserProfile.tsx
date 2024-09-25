81e6a3a038524ef187e0d2c195afebcd9111f4b8
Pilot Context <pilot.context@cryptitalk.com>
2024-09-25 18:03:43 +0000

    better evm address display

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -7,17 +7,18 @@ interface UserProfileProps {
   username: string;
   isVerified: boolean;
   evmAddress?: string;
-  onClick: () => void; // Add this line
+  onClick: () => void;
 }
 
 const UserProfile: React.FC<UserProfileProps> = ({ firstName, lastName, username, isVerified, evmAddress, onClick }) => (
-  <div className="flex items-center space-x-2 pt-4" onClick={onClick}> {/* Add onClick here */}
+  <div className="flex items-center space-x-2 pt-4" onClick={onClick}>
     <div className="p-1 rounded-lg bg-[#1d2025]">
       <Hamster size={24} className="text-[#d4d4d4]" />
     </div>
     <div>
       <p className="text-sm">
-        {firstName} {lastName} {isVerified ? `(${evmAddress?.slice(0, 6)})` : `(${username})`}
+        {firstName} {lastName}{' '}
+        {isVerified && evmAddress ? `(${evmAddress.slice(0, 6)}...${evmAddress.slice(-4)})` : `(${username})`}
       </p>
     </div>
   </div>
